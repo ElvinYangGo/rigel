c2c14a2861ed5cd2957864bdf11e0298be839ae6
elvin yang go <elvin.yang.go@gmail.com>
2012-07-05 22:08:35 +0800

    change name from redis_table_writer to redis_key_writer

--- generator/generator_main.py
+++ generator/generator_main.py
@@ -1,5 +1,5 @@
 import json
-from generator.redis_table_writer import RedisTableWriter
+from generator.redis_key_writer import RedisKeyWriter
 from generator.redis_accessor_generator.redis_accessor_writer import RedisAccessorWriter
 from generator.plain_class_generator.plain_table_array_writer import PlainTableArrayWriter
 from generator.plain_class_accessor_generator.plain_class_accessor_writer import PlainClassAccessorWriter
@@ -10,8 +10,8 @@ if __name__ == '__main__':
 	for table_desc in table_desc_array:
 		print table_desc
 
-	redis_table_writer = RedisTableWriter('../redis_client/redis_table.py', table_desc_array)
-	redis_table_writer.write()
+	redis_key_writer = RedisKeyWriter('../redis_client/redis_key.py', table_desc_array)
+	redis_key_writer.write()
 	redis_accessor_writer = RedisAccessorWriter('../redis_client/redis_accessor.py', table_desc_array)
 	redis_accessor_writer.write()
 	plain_table_array_writer = PlainTableArrayWriter('../plain_class/', table_desc_array)
