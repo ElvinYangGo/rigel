1eb2bba3df83d959a15e0588b2045eb1fed9e562
elvin yang go <elvin.yang.go@gmail.com>
2012-07-08 18:35:07 +0800

    add plain_class_name and redis_accessor_name for public use

---
+++ generator/plain_class_name.py
@@ -0,0 +1,37 @@
+class PlainClassName(object):
+	def get_file_name(self, table_name):
+		return table_name
+
+	def get_list_file_name(self, table_name):
+		return '{}_manager'.format(table_name)
+
+	def get_map_class_name(self, table_name):
+		word_list = table_name.split('_')
+		capitalized_word_list = [word.capitalize() for word in word_list]
+		class_name = ''.join(capitalized_word_list)
+		return class_name
+
+	def get_list_class_name(self, table_name):
+		word_list = table_name.split('_')
+		capitalized_word_list = [word.capitalize() for word in word_list]
+		capitalized_word_list.append('Manager')
+		class_name = ''.join(capitalized_word_list)
+		return class_name
+
+	def get_map_class_builder_function_name(self, table_name):
+		return 'build_{}_from_{}_dict'.format(
+			table_name,
+			table_name
+			)
+
+	def get_map_dict_builder_function_name(self, table_name):
+		return 'build_{}_dict_from_{}'.format(
+			table_name,
+			table_name
+			)
+
+	def get_map_field_getter_function_name(self, field_name):
+		return 'get_{}'.format(field_name)
+
+	def get_map_field_setter_function_name(self, field_name):
+		return 'set_{}'.format(field_name)
---
+++ generator/redis_accessor_name.py
@@ -0,0 +1,24 @@
+class RedisAccessorName(object):
+	def get_map_getter_function_name(self, table_name):
+		return 'get_{}'.format(table_name)
+
+	def get_map_setter_function_name(self, table_name):
+		return 'set_{}'.format(table_name)
+
+	def get_map_field_getter_function_name(self, table_name, field_name):
+		return 'get_{}_table_{}'.format(table_name, field_name)
+
+	def get_map_field_setter_function_name(self, table_name, field_name):
+		return 'set_{}_table_{}'.format(table_name, field_name)
+
+	def get_list_getter_function_name(self, table_name):
+		return 'get_{}_list'.format(table_name)
+
+	def get_list_adder_function_name(self, table_name):
+		return 'add_{}'.format(table_name)
+
+	def get_list_remover_function_name(self, table_name):
+		return 'remove_{}'.format(table_name)
+
+	def get_dict_list_getter_function_name(self, table_name):
+		return 'get_{}_dict_list'.format(table_name)
