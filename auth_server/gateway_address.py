7c464b04f5db7179e0a579d3eec591145e768495
elvin yang go <elvin.yang.go@gmail.com>
2012-10-06 16:32:54 +0800

    use with context to open gateway_address.json

--- auth_server/gateway_address.py
+++ auth_server/gateway_address.py
@@ -2,8 +2,8 @@ import json
 
 class GatewayAddress(object):
 	def __init__(self, gateway_address_file_name):
-		gateway_address_file = open(gateway_address_file_name)
-		self.gateway_address_config = json.load(gateway_address_file)
+		with open(gateway_address_file_name) as f:
+			self.gateway_address_config = json.load(f)
 
 	def get_wan_ip(self, server_name):
 		if self.gateway_address_config.has_key(server_name):
