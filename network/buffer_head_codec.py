e2937bd1ac981a00b9e4a35261290a028c886ccf
elvin yang go <elvin.yang.go@gmail.com>
2013-05-28 23:40:13 +0800

    change variable name, MESSAGE_ID_LENGTH should be HEAD_LENGTH

--- network/buffer_head_codec.py
+++ network/buffer_head_codec.py
@@ -2,18 +2,18 @@ import struct
 from network.channel_buffer import ChannelBuffer
 
 class BufferHeadCodec(object):
-	MESSAGE_ID_LENGTH = struct.calcsize('!i')
+	HEAD_LENGTH = struct.calcsize('!H')
 
 	def handle_upstream(self, channel_buffer, **kwargs):
-		if BufferHeadCodec.MESSAGE_ID_LENGTH < channel_buffer.readable_bytes():
-			packet_length, = struct.unpack('!i', channel_buffer.get_data(BufferHeadCodec.MESSAGE_ID_LENGTH))
-			if BufferHeadCodec.MESSAGE_ID_LENGTH + packet_length <= channel_buffer.readable_bytes():
-				channel_buffer.skip_data(BufferHeadCodec.MESSAGE_ID_LENGTH)
+		if BufferHeadCodec.HEAD_LENGTH < channel_buffer.readable_bytes():
+			packet_length, = struct.unpack('!H', channel_buffer.get_data(BufferHeadCodec.HEAD_LENGTH))
+			if BufferHeadCodec.HEAD_LENGTH + packet_length <= channel_buffer.readable_bytes():
+				channel_buffer.skip_data(BufferHeadCodec.HEAD_LENGTH)
 				return ChannelBuffer(channel_buffer.read_data(packet_length))
 		return None
 
 	def handle_downstream(self, channel_buffer, **kwargs):
 		raw_buffer = channel_buffer.get_all_data()
-		encoded_data = struct.pack('!i' + str(len(raw_buffer)) + 's', len(raw_buffer), raw_buffer)
+		encoded_data = struct.pack('!H' + str(len(raw_buffer)) + 's', len(raw_buffer), raw_buffer)
 		return ChannelBuffer(encoded_data)
 
